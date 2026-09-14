72b5f1b1a3f789f300e7aa899931512fee8f8d51
Maxim <maaximb.14@gmail.com>
2026-09-14 18:16:37 +0200

    Costa sea trips V0.1

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static(__dirname))
+
+app.get("/", function(request, responce){
+
+    responce.sendFile(__dirname + ("/index.html"))
+})
+
+app.get("/actividades", function(request, responce){
+    responce.sendFile(__dirname + ("/actividades.html"))
+})
+
+app.get("/about", function(request, responce){
+    responce.sendFile(__dirname + ("/about.html"))
+})
+app.get("/faq", function(request, responce){
+    responce.sendFile(__dirname + "/FAQ.html")
+})
+app.listen(3000, function() {
+    console.log("Server running on port 3000")
+})
